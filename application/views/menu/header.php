0686cc6ed373602270bcb6a05f2575261db0b0d6
t4pama01@students.oamk.fi <t4pama01@students.oamk.fi>
2016-11-15 12:24:58 +0200

    Toimiva proto

--- application/views/menu/header.php
+++ application/views/menu/header.php
@@ -7,5 +7,5 @@
 
 <ul>
 <li><a href="<?php echo site_url('home');?>">Pääsivu</a></li>
-<li><a href="<?php echo site_url('Tiedot/naytaTiedot'); ?>">Tiedot</a></li>
+<li><a href="<?php echo site_url('tiedot/naytaTiedot'); ?>">Tiedot</a></li>
 </ul>
